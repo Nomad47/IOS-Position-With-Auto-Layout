4c2af67debd52fe2da436a63dfe38d0ad0b708e6
19AF OSS <mark.thrower.2@us.af.mil>
2019-07-20 19:58:02 -0500

    First Iteration

---
+++ LessonTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LessonTwo
+//
+//  Created by Mark on 7/18/19.
+//  Copyright © 2019 19AF OSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
